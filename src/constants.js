ffe5095039fd9bffce036e352744167789e83e72
Mutinda-Kioko <66808040+Mutinda-Kioko@users.noreply.github.com>
2021-12-16 16:56:03 +0000

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+const CONTRACT_ADDRESS = '0x845AC04123aF2fe4192b2f74e2cA8B431966895B';
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
